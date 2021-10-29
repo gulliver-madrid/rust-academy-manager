9c1d382a4b4dc5d59fa782b969ffbf0c7aa51602
GulliverMadrid <49131885+GulliverMadrid@users.noreply.github.com>
2021-10-29 13:21:48 +0200

    Refactorizo shared_menus.rs usando el operador '?'

--- src/shared_menus.rs
+++ src/shared_menus.rs
@@ -10,12 +10,7 @@ pub fn extraer_opcion<'a, OpcionMenu>(
     // Recibe una string que idealmente contendra un numero
     // y a partir de ese numero devolveremos la OpcionMenu
     // correspondiente, segun el orden del vector de objetos ItemMenu
-    let posible: Option<usize> = eleccion.parse().ok();
-    if let Some(num_opcion) = posible {
-        let posible = items_menu.get(num_opcion - 1);
-        if let Some(item_elegido) = posible {
-            return Some(&item_elegido.opcion);
-        }
-    }
-    return None;
+    let num_opcion: usize = eleccion.parse().ok()?;
+    let &item_elegido = &items_menu.get(num_opcion - 1)?;
+    return Some(&item_elegido.opcion);
 }
