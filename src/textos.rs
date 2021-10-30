e8b235e0555a5706dedc214c3ed91c26cd80d227
GulliverMadrid <49131885+GulliverMadrid@users.noreply.github.com>
2021-10-30 12:56:04 +0200

    Refactorizo menu_anadir_profe.rs

--- src/textos.rs
+++ src/textos.rs
@@ -7,3 +7,5 @@ pub const LISTA_ASIGNATURAS: &str = "Lista de asignaturas";
 
 pub const INTRODUCE_NOMBRE_PROFESOR: &str ="Introduce el nombre de un nuevo profesor (dejalo en blanco para volver al menu anterior)";
 pub const INTRODUCE_NOMBRE_ASIGNATURA: &str ="Introduce el nombre de la nueva asignatura (dejalo en blanco para volver al menu anterior)";
+
+pub const ERROR_NO_PROFESOR: &str = "Error: no se encontró ningún profesor";
