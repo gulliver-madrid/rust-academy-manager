7fceb861ef26b9fb31b140782194b52fa0432165
GulliverMadrid <49131885+GulliverMadrid@users.noreply.github.com>
2022-02-05 20:46:58 +0100

    Utilizo Result también para AddTeacherUseCase

--- src/application/application.rs
+++ src/application/application.rs
@@ -1,22 +1,19 @@
-use crate::{errors::SimpleError, repository::Repository};
+use crate::{errors::SimpleResult, repository::Repository};
 
-use super::{
-    add_teacher::AddTeacherUseCase, remove_teacher::RemoveTeacherUseCase,
-
-};
+use super::{add_teacher::AddTeacherUseCase, remove_teacher::RemoveTeacherUseCase};
 
 pub struct Application {
     pub repository: Repository,
 }
 
 impl Application {
-    pub fn anadir_nuevo_profesor(&mut self, nombre: String) {
+    pub fn anadir_nuevo_profesor(&mut self, nombre: &str) -> SimpleResult {
         AddTeacherUseCase {
             repository: &mut self.repository,
         }
-        .anadir_nuevo_profesor(nombre);
+        .anadir_nuevo_profesor(nombre.to_string())
     }
-    pub fn eliminar_profesor(&mut self, nombre: &str) -> Result<(), SimpleError> {
+    pub fn eliminar_profesor(&mut self, nombre: &str) -> SimpleResult {
         RemoveTeacherUseCase {
             repository: &mut self.repository,
         }
