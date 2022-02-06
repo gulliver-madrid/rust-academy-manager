4da154bd4204fe3df46cefccc9bdf38351a21895
GulliverMadrid <49131885+GulliverMadrid@users.noreply.github.com>
2022-02-06 10:09:14 +0100

    Encapsulo Repository dentro de Application

--- src/application/application.rs
+++ src/application/application.rs
@@ -1,18 +1,32 @@
 use crate::{
+    dominio::{Asignaturas, Profesores},
     errors::{SimpleError, SimpleResult},
     repository::Repository,
 };
 
 use super::{
     add_subject::AddSubjectUseCase, add_teacher::AddTeacherUseCase,
-    remove_subject::RemoveSubjectUseCase, remove_teacher::RemoveTeacherUseCase, assign_teacher_to_subject::AssignTeacherToSubjectUseCase, get_subject_index_by_name::GetSubjectIndexByNameUseCase,
+    assign_teacher_to_subject::AssignTeacherToSubjectUseCase,
+    get_subject_index_by_name::GetSubjectIndexByNameUseCase,
+    remove_subject::RemoveSubjectUseCase, remove_teacher::RemoveTeacherUseCase,
 };
 
 pub struct Application {
-    pub repository: Repository,
+    repository: Repository,
 }
 
 impl Application {
+    pub fn new(repository: Repository) -> Self {
+        Self { repository }
+    }
+    /// Carga las asignaturas en el Modelo si es necesario
+    pub fn load_subjects(&mut self) -> () {
+        self.repository.load_subjects();
+    }
+    /// Carga los profesores en el Modelo si es necesario
+    pub fn load_profesores(&mut self) -> () {
+        self.repository.load_profesores();
+    }
     pub fn anadir_nuevo_profesor(&mut self, nombre: &str) -> SimpleResult {
         AddTeacherUseCase {
             repository: &mut self.repository,
@@ -57,5 +71,11 @@ impl Application {
             repository: &mut self.repository,
         }
         .asignar_profesor_a_asignatura(index_asignatura, id_profesor)
+    }
+    pub fn get_asignaturas(&self) -> &Asignaturas {
+        &self.repository.modelo.asignaturas.as_ref().unwrap()
+    }
+    pub fn get_teachers(&self) -> &Profesores {
+        &self.repository.modelo.profesores.as_ref().unwrap()
     }
 }
