ceb44de302a533aa4bbab69860dc9f8441ef3eb4
GulliverMadrid <49131885+GulliverMadrid@users.noreply.github.com>
2022-02-11 12:58:02 +0100

    Hago MockConsole más general

--- src/tests/test_menu.rs
+++ src/tests/test_menu.rs
@@ -1,31 +1,44 @@
-use crate::{
-    consola::InnerConsole,
-    dominio::{Asignatura, Asignaturas, Profesor, Profesores},
-    menus::ITEMS_MENU_DATA,
-    repository::PersistenciaTrait,
-};
+#[cfg(test)]
+use std::cell::RefCell;
 
 #[cfg(test)] // El compilador solo lo detecta en modo test
 use crate::{
     application::Application,
     components::Control,
     consola,
+    consola::InnerConsole,
+    dominio::{Asignatura, Asignaturas, Profesor, Profesores},
+    menus::ITEMS_MENU_DATA,
     menus::{Menu, MenuPrincipal},
+    repository::PersistenciaTrait,
 };
 
-pub struct MockConsole {}
+#[cfg(test)]
+pub struct MockConsole {
+    pub provided_inputs: RefCell<Vec<String>>,
+}
 
+#[cfg(test)]
+impl MockConsole {
+    fn new() -> Self {
+        Self {
+            provided_inputs: RefCell::new(Vec::<String>::new()),
+        }
+    }
+}
+#[cfg(test)]
 impl InnerConsole for MockConsole {
     fn clear_screen(&self) {}
     fn get_input(&self) -> String {
-        let num_opciones = ITEMS_MENU_DATA.len();
-        let ultima_opcion = num_opciones.to_string();
-        ultima_opcion
+        self.provided_inputs.borrow_mut().pop().unwrap()
     }
     fn mostrar(&self, _texto: &str) {}
 }
 
+#[cfg(test)]
 pub struct MockPersistencia {}
+
+#[cfg(test)]
 impl PersistenciaTrait for MockPersistencia {
     fn save_profesores(&self, _profesores: &Profesores) {}
     fn save_asignaturas(&self, _asignaturas: &Asignaturas) {}
@@ -36,13 +49,24 @@ impl PersistenciaTrait for MockPersistencia {
         return Vec::<Asignatura>::new();
     }
 }
-
+#[cfg(test)]
+fn get_last_option_main_menu() -> String {
+    let num_opciones = ITEMS_MENU_DATA.len();
+    let ultima_opcion = num_opciones.to_string();
+    ultima_opcion
+}
 #[test]
 fn salir_desde_menu_principal() {
     let persistencia = MockPersistencia {};
     let application = Application::new(Box::new(persistencia));
+    let provided_input = get_last_option_main_menu();
+    let mock_console = MockConsole::new();
+    mock_console
+        .provided_inputs
+        .borrow_mut()
+        .push(provided_input);
     let consola = consola::Consola {
-        inner_console: Box::new(MockConsole {}),
+        inner_console: Box::new(mock_console),
     };
     let mut control = Control {
         consola,
