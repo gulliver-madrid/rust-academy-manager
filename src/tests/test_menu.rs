b519325931b1182aa9f5198a13d08c2522158323
GulliverMadrid <49131885+GulliverMadrid@users.noreply.github.com>
2022-02-13 13:51:28 +0100

    Update test_menu.rs

--- src/tests/test_menu.rs
+++ src/tests/test_menu.rs
@@ -36,17 +36,20 @@ impl InnerConsole for MockConsole {
 }
 
 #[cfg(test)]
-pub struct MockPersistencia {}
+pub struct MockPersistencia {
+    pub mock_profesores: Vec<Profesor>,
+    pub mock_asignaturas: Vec<Asignatura>,
+}
 
 #[cfg(test)]
 impl PersistenciaTrait for MockPersistencia {
     fn save_profesores(&self, _profesores: &Profesores) {}
     fn save_asignaturas(&self, _asignaturas: &Asignaturas) {}
     fn load_profesores(&self) -> Profesores {
-        return Vec::<Profesor>::new();
+        return self.mock_profesores.clone();
     }
     fn load_subjects(&self) -> Asignaturas {
-        return Vec::<Asignatura>::new();
+        return self.mock_asignaturas.clone();
     }
 }
 #[cfg(test)]
@@ -57,7 +60,10 @@ fn get_last_option_main_menu() -> String {
 }
 #[test]
 fn salir_desde_menu_principal() {
-    let persistencia = MockPersistencia {};
+    let persistencia = MockPersistencia {
+        mock_profesores: Vec::<Profesor>::new(),
+        mock_asignaturas: Vec::<Asignatura>::new(),
+    };
     let application = Application::new(Box::new(persistencia));
     let provided_input = get_last_option_main_menu();
     let mock_console = MockConsole::new();
