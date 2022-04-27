ddba2ff2fd893d2f6b801959cb59c9919ae9bd2d
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2022-04-27 12:45:13 +0200

    Agrupo los usecases en su propio directorio

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,10 +1,5 @@
 mod application;
 
-mod add_teacher;
-mod remove_teacher;
-mod add_subject;
-mod remove_subject;
-mod assign_teacher_to_subject;
-mod get_subject_index_by_name;
+mod usecases;
 
 pub use application::Application;
