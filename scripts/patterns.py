57aac86468c98b59a36333f56ce4d4142e065004
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2022-04-30 13:51:25 +0200

    Improve patterns naming in validate script

--- scripts/patterns.py
+++ scripts/patterns.py
@@ -6,16 +6,17 @@ optional_part_of_key_pattern = r"(?:[.]\w+)"
 key_pattern = fr"[\w]+{optional_part_of_key_pattern}?" 
 
 text_pattern = fr'\({optional_spaces}{menu_option_variant},{optional_spaces}"({key_pattern})"[,)]'
+# example: (MenuOption::Teachers, "menu_options.teacher")
+menu_option_pattern: re.Pattern[str] = re.compile(text_pattern, re.ASCII)
 
-# example: MenuOption::Teachers, "menu_options.teacher")
-pattern_2: re.Pattern[str] = re.compile(text_pattern, re.ASCII)
+def test_match_menu_option_pattern_1():
+    assert menu_option_pattern.search('(MenuOption::ShowList, "subjects_menu_options.show_list"),')
 
-def test_match_pattern_2_1():
-    assert pattern_2.search('(MenuOption::ShowList, "subjects_menu_options.show_list"),')
-def test_match_pattern_2_2():
-    assert pattern_2.search('''(
+def test_match_menu_option_pattern_2():
+    assert menu_option_pattern.search('''(
         MenuOption::AssignTeacher,
         "subjects_menu_options.assign_teacher",
     ),''')
-def test_no_match_pattern_2():
-    assert not pattern_2.search('("subjects_menu_options.assign_teacher"')
+
+def test_no_match_menu_option_pattern():
+    assert not menu_option_pattern.search('("subjects_menu_options.assign_teacher"')
