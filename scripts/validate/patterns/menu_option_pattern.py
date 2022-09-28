ab1665fb848d634286dde8cc11251f8594466bb6
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2022-09-28 11:58:53 +0200

    [Scripts/Feat] Improve translation keys detection
    Validate script can now detect translation parametrized calls.

--- scripts/validate/patterns/menu_option_pattern.py
+++ scripts/validate/patterns/menu_option_pattern.py
@@ -1,22 +1,27 @@
 import re
 
+# Patron para detectar cadenas en las MenuOptions
+
 optional_spaces = r"\s*"
 menu_option_variant = r"MenuOption::[A-Z]\w*"
 optional_part_of_key_pattern = r"(?:[.]\w+)"
-key_pattern = fr"[\w]+{optional_part_of_key_pattern}?" 
+key_pattern = fr"[\w]+{optional_part_of_key_pattern}?"
 
 text_pattern = fr'\({optional_spaces}{menu_option_variant},{optional_spaces}"({key_pattern})"[,)]'
 # example: (MenuOption::Teachers, "menu_options.teacher")
 menu_option_pattern: re.Pattern[str] = re.compile(text_pattern, re.ASCII)
 
-def test_match_menu_option_pattern_1():
+
+def test_match_menu_option_pattern_1() -> None:
     assert menu_option_pattern.search('(MenuOption::ShowList, "subjects_menu_options.show_list"),')
 
-def test_match_menu_option_pattern_2():
+
+def test_match_menu_option_pattern_2() -> None:
     assert menu_option_pattern.search('''(
         MenuOption::AssignTeacher,
         "subjects_menu_options.assign_teacher",
     ),''')
 
-def test_no_match_menu_option_pattern():
+
+def test_no_match_menu_option_pattern() -> None:
     assert not menu_option_pattern.search('("subjects_menu_options.assign_teacher"')
--- scripts/validate/patterns/simple_pattern.py
+++ scripts/validate/patterns/simple_pattern.py
@@ -1,9 +1,30 @@
 import re
 from .menu_option_pattern import menu_option_pattern
-
-simple_pattern = re.compile(r"""
-            [^a-zA-Z]       # Some non alphabetic character (prevent format! false positives)
-            t!\("           # t!("
-            ([^"]+)         # the translation key as a captured group
-            "\)             # ")
+NO_CLOSE_PAREN_CHAR = r'[^)]'
+simple_pattern = re.compile(fr"""
+    [^a-zA-Z]       # Some non alphabetic character (prevent format! false positives)
+    t!\("           # t!("
+    ([^"]+)         # the translation key as a captured group
+    "               # "
+    (?:,
+        {NO_CLOSE_PAREN_CHAR}+
+    )?               # optional non-capturing group with a comma and several characters more, excepting close parens.
+            \)              # )
 """, re.VERBOSE)
+
+
+
+def test_match_simple_pattern_1() -> None:
+    KEY = "una_key"
+    m = simple_pattern.search(f'let s = t!("{KEY}")')
+    assert m
+    assert len(m.groups()) == 1
+    assert m.groups()[0] == KEY
+
+
+def test_match_simple_pattern_2() -> None:
+    KEY = "una_key_con_arg_${name}_"
+    m = simple_pattern.search(f'let s = t!("{KEY}", name=name)')
+    assert m
+    assert len(m.groups()) == 1
+    assert m.groups()[0] == KEY
